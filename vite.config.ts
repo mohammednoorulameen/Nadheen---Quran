442c4af0ced1f22bf270751bbd4b20aeee0d6f30
mohammednoorulameen <noorulameen8606@gmail.com>
2026-01-16 15:10:53 +0530

    PWA production build ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(), VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico'],
+      manifest: {
+        name: 'nadheenQuran',
+        short_name: 'nadheen',
+        description: 'My React PWA Application',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/alquran.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/alquran.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })],
   resolve:{
     alias:{
       "@": path.resolve(__dirname, "./src")
